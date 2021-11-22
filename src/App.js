cd2132beeed535576865ad071a943aa3aead2494
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2021-11-22 11:41:54 -0300

    Save Changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+// foi usado como base para essa página o código do esquenta do dia 19/11
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={ Login } />
+        <Route path="/carteira" component={ Wallet } />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,78 @@
+// foi usado como base para essa página o código do esquenta do dia 19/11
 import React from 'react';
+import { Redirect } from 'react-router';
+
+// links usados para fazer verificação regex: https://www.youtube.com/watch?v=QxjAOSUQjP0 &&
+// https://stackoverflow.com/questions/46155/whats-the-best-way-to-validate-an-email-address-in-javascript
+const regexEmailValidation = /^([a-z\d\]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      validLogin: false,
+      redirect: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({ [target.name]: target.value });
+    const { email, password } = this.state;
+    const minLengthPassword = 5;
+    console.log(password.length);
+    if (regexEmailValidation.test(email)
+     && password.length >= minLengthPassword) {
+      this.setState({ validLogin: true });
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { validLogin } = this.state;
+    if (validLogin === true) {
+      this.setState({ redirect: true });
+    }
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, redirect, password } = this.state;
+    return (redirect ? <Redirect to="/carteira" /> : (
+      <form id="login" onSubmit={ this.handleSubmit }>
+        <label htmlFor="email-input">
+          Email:
+          <br />
+          <input
+            data-testid="email-input"
+            type="text"
+            value={ email }
+            name="email"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="password-input">
+          Senha:
+          <br />
+          <input
+            data-testid="password-input"
+            type="password"
+            value={ password }
+            name="password"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="submit"
+        >
+          Entrar
+        </button>
+      </form>)
+    );
   }
 }
 
