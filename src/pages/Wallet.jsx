e90e82a45219d3efeb75367d08c6a4f7de1b68ba
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2021-11-24 21:17:47 -0300

    4° Requisite_Save Changes

---
+++ src/pages/Wallet.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
+import WalletForm from '../components/WalletForm';
+
+class Wallet extends React.Component {
+  render() {
+    const { getEmail, getExpenses } = this.props;
+    return (
+      <>
+        <header>
+          <h1>TrybeWallet</h1>
+          <h2 data-testid="email-field">{`Email: ${getEmail}`}</h2>
+          <span
+            data-testid="total-field"
+          >
+            {`Despesa Total: ${getExpenses.length === 0 ? 0 : 1}`}
+          </span>
+          <span data-testid="header-currency-field">
+          &nbsp;BRL
+          </span>
+        </header>
+        <br />
+        <WalletForm />
+      </>);
+  }
+}
+
+Wallet.propTypes = {
+  getEmail: PropTypes.string,
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  getEmail: state.user.email,
+  getExpenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Wallet);
